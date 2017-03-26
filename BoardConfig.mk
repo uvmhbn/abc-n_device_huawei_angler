BenzoRoms/device_huawei_angler

Add TARGET_CPU_FEATURES
Change-Id: If19f46dbadd430595ece13a1e4bd639407790c40
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

BoardConfig.mk
@@ -22,12 +22,14 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_FEATURES := crc
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
+TARGET_2ND_CPU_FEATURES := div,atomic_ldrd_strd
 
 ENABLE_CPUSETS := true
 

uvmhbn/abc-n_device_huawei_angler

BoardConfig.mk
@@ -22,12 +22,14 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_FEATURES := a53
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53.a57
+TARGET_2ND_CPU_FEATURES := div,atomic_ldrd_strd
 
 ENABLE_CPUSETS := true
 